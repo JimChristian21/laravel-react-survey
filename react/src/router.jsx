4ca20f578edd99ac81c237bc36b402b6e148417b
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-07 21:39:40 +0800

    Init Dashboard, Login, Signup, and Surveys Component. Removed the App.jsx

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,27 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from './App.jsx';
+import Dashboard from './views/Dashboard.jsx';
+import Login from './views/Login.jsx';
+import Signup from './views/Signup.jsx';
+import Surveys from './views/Surveys.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
-    }
+        element: <Dashboard />
+    },
+    {
+        path: '/surveys',
+        element: <Surveys />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Signup />
+    },
 ]);
 
 export default router;
