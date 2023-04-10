fa15dfca32bfd1d1acac163e874a8b7aa7d7d745
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-10 20:59:50 +0800

    Added survey create form

--- react/src/components/core/TButton.jsx
+++ react/src/components/core/TButton.jsx
@@ -7,55 +7,73 @@ export default function TButton({
     href = '',
     link = false,
     target = "_blank",
-		onClick = () => {},
+	onClick = () => {},
     children
 }) {
+	let classes = [
+		"flex",
+		"items-center",
+		"whitespace-nowrap",
+		"text-sm",
+		"border",
+		"border-2",
+		"border-transparent",
+	  ];
 
-	console.log(circle);
-
-  let classes = [
-    "flex",
-    "whitespace-nowrap",
-    "text-sm",
-    "border",
-    "border-2",
-    "border-transparent"
-  ];
-
-	classes = link
-		? [
-      ...classes,
-      "transition-colors",
-			`text-${color}-500`,
-			`focus:border-${color}-500`,
-    ]
-		: [
-			...classes,
-			"text-white",
-			"focus:ring-2",
-			"focus:ring-offset-2",
-			`bg-${color}-600`,
-			`hover:bg-${color}-700`,
-			`focus:ring-${color}-500`,
+	if (link) {
+		classes = [...classes, "transition-colors"];
+	
+		switch (color) {
+		  case "indigo":
+			classes = [...classes, "text-indigo-500", "focus:border-indigo-500"];
+			break;
+		  case "red":
+			classes = [...classes, "text-red-500", "focus:border-red-500"];
+		}
+	  } else {
+		classes = [...classes, "text-white", "focus:ring-2", "focus:ring-offset-2"];
+	
+		switch (color) {
+		  case "indigo":
+			classes = [
+			  ...classes,
+			  "bg-indigo-600",
+			  "hover:bg-indigo-700",
+			  "focus:ring-indigo-500",
+			];
+			break;
+		  case "red":
+			classes = [
+			  ...classes,
+			  "bg-red-600",
+			  "hover:bg-red-700",
+			  "focus:ring-red-500",
+			];
+			break;
+		  case "green":
+			classes = [
+			  ...classes,
+			  "bg-emerald-500",
+			  "hover:bg-emerald-600",
+			  "focus:ring-emerald-400",
+			];
+			break;
+		}
+	  }
+	
+	  if (circle) {
+		classes = [
+		  ...classes,
+		  "h-8",
+		  "w-8",
+		  "items-center",
+		  "justify-center",
+		  "rounded-full",
+		  "text-sm",
 		];
-
-	classes = circle 
-		? [
-			...classes,
-			"h-8",
-			"w-8",
-			"items-center",
-			"justify-center",
-			"rounded-full",
-			"text-sm",
-		] 
-		: [
-			...classes,
-			"p-0",
-			"py-2",
-			"px-4",
-			"rounded-md",
-		];
+	  } else {
+		classes = [...classes, "p-0", "py-2", "px-4", "rounded-md"];
+	  }
 
   return (
     <>
