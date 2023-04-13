a9cc8fd77f57a096c5d9a33156a8753d7526d933
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-14 00:00:49 +0800

    Added Sign in

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,6 +1,40 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axiosClient from '../axios';
+import { useStateContext } from '../context/ContextProvider';
 
-export default function App() {
+export default function Login() {
+  const { setCurrentUser, setUserToken } = useStateContext();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState('');
+  const [error, setError] = useState({__html: ''});
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setError({ __html: '' });
+
+    axiosClient.post('/login', {
+      email,
+      password,
+      remember_me: rememberMe
+    }).then(({data}) => {
+      setCurrentUser(data.user);
+      setUserToken(data.token);
+    }).catch((error) => {
+      
+      if (error.response) {
+        const finalErrors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next] ,[]);
+
+        setError({ __html: finalErrors.join('<br>')});
+      }
+
+      // console.error(error);
+    });
+  }
+
   return (
     <>
 			<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
@@ -8,11 +42,20 @@ export default function App() {
       </h2>
       <p className="mt-2 text-center text-sm text-gray-600">
         Or{' '}
-				<a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-					start your 14-day free trial
-				</a>
+				<Link to="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
+					Sign up for free
+				</Link>
       </p>
-      <form className="mt-8 space-y-6" action="#" method="POST">
+
+      {error.__html && (
+        <div 
+          className='bg-red-500 rounded py-2 px-3 text-white'
+          dangerouslySetInnerHTML={error}
+        >
+
+        </div>
+      )}
+      <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={onSubmit}>
         <input type="hidden" name="remember" defaultValue="true" />
         <div className="-space-y-px rounded-md shadow-sm">
           <div>
@@ -23,6 +66,8 @@ export default function App() {
               id="email-address"
               name="email"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               autoComplete="email"
               required
               className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -37,6 +82,8 @@ export default function App() {
               id="password"
               name="password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               autoComplete="current-password"
               required
               className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -55,11 +102,6 @@ export default function App() {
             <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
               Remember me
             </label>
-          </div>
-          <div className="text-sm">
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              Forgot your password?
-            </a>
           </div>
         </div>
         <div>
